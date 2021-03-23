b38bc3bf68bff821317af6bc335d7df0c47b6d86
Luiz Reis <luizcgr@gmail.com>
2021-03-23 13:17:10 -0300

    Initial commit

---
+++ src/__tests__/either.test.ts
@@ -0,0 +1,67 @@
+import { Either, left, right } from "../either";
+
+describe("Either", () => {
+  test("dado um left, então verifica isLeft", () => {
+    let result: Either<Error, string>;
+    result = left(new Error("teste"));
+    expect(result.isRight()).toBeFalsy();
+  });
+
+  test("dada um right, então verifica isRight", () => {
+    let result: Either<Error, boolean> = right(true);
+    expect(result.isRight()).toBeTruthy();
+  });
+
+  test("dado um right, então verifica valor contido", () => {
+    let result: Either<Error, number> = right(12345);
+    expect(result.getRight()).toBe(12345);
+    expect(result.getLeft()).toBeNull();
+  });
+
+  test("dado um left, então verifica erro", () => {
+    let result: Either<Error, number> = left(new Error("erro pje"));
+    expect(result.getLeft()).toBeInstanceOf(Error);
+    expect(result.getRight()).toBeNull();
+  });
+
+  test("dado um left, então verifica mensagem de erro", () => {
+    let result: Either<Error, number> = left(new Error("erro pje"));
+    expect(result.getLeft().message).toBe("erro pje");
+  });
+
+  test("dado um fold right, então verifica callback chamado", () => {
+    const rightFn = jest.fn();
+    const leftFn = jest.fn();
+    let result: Either<Error, number> = right(1);
+    result.fold(leftFn, rightFn);
+    expect(rightFn).toBeCalledTimes(1);
+  });
+
+  test("dado um fold left, então verifica callback chamado", () => {
+    const rightFn = jest.fn();
+    const leftFn = jest.fn();
+    let result: Either<Error, number> = left(new Error("error"));
+    result.fold(leftFn, rightFn);
+    expect(leftFn).toBeCalledTimes(1);
+  });
+
+  test("dado um fold right que retorna um número, então verifica retorno do fold", async () => {
+    const f = (): Either<Error, number> => right(1);
+    let result: Either<Error, number> = f();
+    let n = result.fold<number>(
+      (err) => 0,
+      (num) => 100
+    );
+    expect(n).toBe(100);
+  });
+
+  test("dado um fold left que retorna um número, então verifica retorno do fold", async () => {
+    const f = (): Either<Error, number> => left(new Error());
+    let result: Either<Error, number> = f();
+    let n = result.fold<number>(
+      (err) => 0,
+      (num) => 100
+    );
+    expect(n).toBe(0);
+  });
+});
---
+++ src/either.ts
@@ -0,0 +1,39 @@
+class Left<L, R> {
+  constructor(private _value: L, _: R) {}
+
+  getLeft = (): L => this._value;
+
+  getRight = (): R => null;
+
+  isRight = (): boolean => {
+    return false;
+  };
+
+  fold<T>(leftFn: (_: L) => T, rightFn: (_: R) => T): T {
+    return leftFn(this._value);
+  }
+}
+
+class Right<L, R> {
+  constructor(_: L, private _value: R) {}
+
+  getRight = (): R => this._value;
+
+  getLeft = (): L => null;
+
+  isRight(): boolean {
+    return true;
+  }
+
+  fold<T>(leftFn: (_: L) => T, rightFn: (_: R) => T): T {
+    return rightFn(this._value);
+  }
+}
+
+export type Either<L, R> = Left<L, R> | Right<L, R>;
+
+export type EitherP<L, R> = Promise<Either<L, R>>;
+
+export const right = <L, R>(value: R) => new Right<L, R>(null, value);
+
+export const left = <L, R>(value: L) => new Left<L, R>(value, null);
